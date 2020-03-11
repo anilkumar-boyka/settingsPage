5ab9734372572be0aa0f4eef9cbf66908079435b
Anil kumar <anilkumar25021995@gmail.com>
2020-03-11 14:04:51 +0530

    setting_component_added

--- src/components/firewall1.vue
+++ src/components/firewall1.vue
@@ -1,38 +1,41 @@
 <template>
   <div class="firewall">
-    <div>
+    <!-- <div>
         <h2>Firewall 1</h2>
-        <b-form >
-          <div class="mx-auto labels" style="width: 500px;">
-            <b-form-group label="Ip address:" >
-                <b-form-input
-                  type="text"
-                  v-model="ip_Info"
-                  required
-                  placeholder="Enter Ip address">
-                </b-form-input>
-            </b-form-group>
-            <b-form-group label="Username:">
-                <b-form-input
-                    type="text"
-                    v-model="username"
-                    required
-                    placeholder="Enter username">
-                </b-form-input>
-            </b-form-group>
-              <b-form-group label="Your password:">
-                <b-form-input type="password"
-                  v-model="password"
-                  required
-                  placeholder="Enter password">
-                </b-form-input>
-              </b-form-group>
-          </div>
-          <!-- <button v-on:click.prevent="saveData()">Save data</button> -->
-          <b-button type="submit" variant="secondary" v-on:click="saveData()" >Update</b-button>
-          <b-button  class="showButton" variant="secondary" v-on:click="showData()">click here to show data</b-button>
-        </b-form>
+          <div class="form" v-if="show_form">
+            <b-form >
+              <div class="mx-auto labels" style="width: 500px;">
+                <b-form-group label="Ip address:" >
+                    <b-form-input
+                      type="text"
+                      v-model="ip_Info"
+                      required
+                      placeholder="Enter Ip address">
+                    </b-form-input>
+                </b-form-group>
+                <b-form-group label="Username:">
+                    <b-form-input
+                        type="text"
+                        v-model="username"
+                        required
+                        placeholder="Enter username">
+                    </b-form-input>
+                </b-form-group>
+                  <b-form-group label="Your password:">
+                    <b-form-input type="password"
+                      v-model="password"
+                      required
+                      placeholder="Enter password">
+                    </b-form-input>
+                  </b-form-group>
+              </div>
+              <b-button type="submit" variant="secondary" v-on:click="updateData()" >Update</b-button>
+              
+            </b-form>
+          </div>  
         <div>
+          <span v-if='show_button'><b-button  class="showButton" variant="secondary" v-on:click="showData()">click here to show data</b-button></span>
+          <span v-if='edit_button'><b-button  class="showButton" variant="secondary" v-on:click="edit()">Edit</b-button></span>
           
           <div class="info" v-if='show'>
             <span>Ip address - {{ip}}<br></span>
@@ -40,66 +43,46 @@
           </div>  
         </div>
       <hr>
-    <!-- <firewall2 /> -->
-    </div>  
+    </div>   -->
+    <ul>
+      <li v-for="(info,index) in infos">
+        <div class="data">
+          <h2>Firewall {{index+1}}</h2>
+          <div class="info">
+            Ip Address - {{info.ip_address}}<br>
+            Username - {{info.passWord}}
+          </div>
+        </div><hr>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 import firebase from 'firebase'
-import Firewall2 from '@/components/firewall2'
 export default {
-  name: 'settings',
-  components: {
-    Firewall2
-  },
+  props : ['infos'],
+  name: 'firewall1',
   data () {
     return {
-      username: '',
-      password: '',
-      ip_Info: '',
-      data:{},
-      show : 0,
-      ip :'',
-      pswd :'',
-      uname : ''
+      // username: '',
+      // password: '',
+      // ip_Info: '',
+      // data:{},
+      // show : 0,
+      // ip :'',
+      // pswd :'',
+      // uname : '',
+      // show_form : 0,
+      // edit_button : 0,
+      // show_button : 0
     }
   },
   methods: {
-    ipInfo: function () {
-      // this.ip_Info = 123
-      console.log('hello')
-
-    },
-    showData: function () {
-      var vm=this;
-      var todoRef = firebase.database().ref("items/");
-      todoRef.on("value", function(snapshot) {
-        console.log(snapshot.val()[0].ip_address)
-        vm.ip = snapshot.val()[0].ip_address;
-        vm.uname = snapshot.val()[0].user_name;
-        vm.show=1;
-      });
-    },
-    saveData:function () {
-      var vm=this;
-      console.log('hello');
-      this.$http.post('https://jsonplaceholder.typicode.com/posts',{
-        userName:vm.username,
-        passWord:vm.password,
-        ipinfo:vm.ip_Info,
-      }).then(data=>console.log(data)).catch(err=>console.log('error is'+err));
-       var info = firebase.database().ref();
-      info.child('items').set([{
-          ip_address: vm.ip_Info,
-          user_name: vm.username,
-          passWord:vm.password
-      
-      }]);
-    }
   },
   mounted(){
-    // this.showData();
+    console.log('inside f1 component');
+    console.log(this.infos);
   }
 }
 
@@ -116,9 +99,10 @@ export default {
   color:white
 }
 h2{
-  margin-bottom:50px;
-  padding-top:30px;
+  margin-bottom:20px;
+  padding-top:20px;
   color:white;
+  font-size : xx-large;
 }
 hr{
   color:white;
@@ -126,14 +110,18 @@ hr{
 }
 .info{
   color : black;
-  margin: 50px;
-  padding:20px;
+  margin: 20px;
+  padding:10px;
   background-color:white;
   font-family: 'Abril Fatface';
-  font-size: xx-large
+  font-size: xx-large;
+  // opacity : 0.9;
 }
 .showButton{
   margin-left: 100px;
+}
+.data{
+  color:white;
 }
 /* span{ */
   /* background-color: grey */
---
+++ src/components/settings.vue
@@ -0,0 +1,44 @@
+<template>
+ <div>
+ 	<firewall1 :infos=info></firewall1>
+ </div>
+</template>
+
+<script>
+import firebase from 'firebase'
+import firewall1 from '@/components/firewall1'
+export default {
+  name: 'settings',
+  components : {
+  	firewall1,
+  },
+  data () {
+    return {
+    	info : []
+    }
+  },
+  mounted() {
+  	  var vm=this;
+  	  var l;
+      var todoRef = firebase.database().ref("items/");
+      todoRef.on("value", function(snapshot) {
+      	l=snapshot.val().length;
+      	console.log('l is'+l)
+      	console.log(snapshot.val())
+      	vm.info = snapshot.val();
+      	console.log('info is')
+      	console.log(vm.info)
+      	// for(i=0;i<l;i++){
+      	// 	vm.info.push({snapshot.val()[i].})
+      	// }
+        // console.log(snapshot.val()[0].ip_address)
+        // vm.ip = snapshot.val()[0].ip_address;
+        // vm.uname = snapshot.val()[0].user_name;
+      });	
+  }
+}  
+</script>
+
+
+<style scoped>
+</style>
